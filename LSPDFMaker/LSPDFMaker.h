7739a4a3cd3ed4887a2d9a51147ce2917b5370c8
Leejay Schmidt <leejayschmidt@leejays-macbook-pro.local>
2016-06-25 16:33:43 -0600

    Initial Commit

---
+++ LSPDFMaker/LSPDFMaker.h
@@ -0,0 +1,19 @@
+//
+//  LSPDFMaker.h
+//  LSPDFMaker
+//
+//  Created by Leejay Schmidt on 2016-06-25.
+//  Copyright © 2016 Leejay Schmidt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LSPDFMaker.
+FOUNDATION_EXPORT double LSPDFMakerVersionNumber;
+
+//! Project version string for LSPDFMaker.
+FOUNDATION_EXPORT const unsigned char LSPDFMakerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LSPDFMaker/PublicHeader.h>
+
+
